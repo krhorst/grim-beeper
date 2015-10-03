c59d63cc461e2b8fbcb9e8b133710c78c5247902
Bill Burke <bill.burke@avant.com>
2015-10-03 11:58:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrimBeeper::Application.config.secret_token = '175db5f0545ceb0373183da8137785f8322169b5400d8dd96d3498981b3a38c7644126a70040f9d880217b1f669ec6289f13e32cfa6d90fdf11c3109efed3082'
